96e1c0d9293f1e86f53330dea8cb3a28b2a65083
Saygachenko <curatoroff@gmail.com>
2023-08-25 20:51:41 +0300

    Added a new trap rotating platform.

--- enc_temp_folder/6537822caa9ad137d0c742df3825c154/MovingPlatform.h
+++ enc_temp_folder/6537822caa9ad137d0c742df3825c154/MovingPlatform.h
@@ -19,24 +19,19 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere, Category = "Moving")
+	UPROPERTY(EditAnywhere, Category = "Moving Platform")
 	FVector PlatformVelocity = FVector(100, 0, 0);
 
-	UPROPERTY(EditAnywhere, Category = "Moving")
+	UPROPERTY(EditAnywhere, Category = "Moving Platform")
 	float MovedDistance = 100;
-
-	UPROPERTY(EditAnywhere, Category = "Rotation")
-	FRotator RotationVelocity;
 
 	FVector StartLocation;
 
 private:
 	void MovePlatform(float DeltaTime);
 	void RotatePlatform(float DeltaTime);
-	
-	bool ShouldPlatforReturn() const;
-	float GetDistanceMoved() const;
 };
